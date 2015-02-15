2399a4640722916ee408a32365cd4229c3172c65
Matt Bostock <matt@mattbostock.com>
2015-02-15 16:48:00 +0000

    Add security middleware
    Use the `unrolled/secure` package to provide some security features,
    namely:
    
    - Content Security Policy header
    - Secure Transport Security header
    - Frame deny header
    - XSS filter header
    - the ability to specify which HTTP hosts are permitted
    
    Secure Transport Security headers are always enabled, even on non-TLS
    connections, because we assume that TLS termination is being performed
    by an upstream forward proxy. Copying cookie data for the purposes of
    spoofing users is trivial over plaintext HTTP, so we assume that TLS
    will always be used.

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/bradfitz/http2"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/pat"
+	"github.com/unrolled/secure"
 	"gitlab.com/mattbostock/timeoff/handler"
 	"gitlab.com/mattbostock/timeoff/middleware/negroni_logrus"
 	"gitlab.com/mattbostock/timeoff/model"
@@ -20,15 +22,16 @@ const (
 
 var (
 	config = &struct {
-		addr    string
-		debug   bool
-		tlsCert string
-		tlsKey  string
+		addr               string
+		allowedHosts       []string
+		debug              bool
+		tlsCert            string
+		tlsKey             string
 	}{
-		addr:    os.Getenv("ADDR"),
-		debug:   os.Getenv("DEBUG") != "",
-		tlsCert: os.Getenv("TLS_CERT"),
-		tlsKey:  os.Getenv("TLS_KEY"),
+		addr:               os.Getenv("ADDR"),
+		debug:              os.Getenv("DEBUG") != "",
+		tlsCert:            os.Getenv("TLS_CERT"),
+		tlsKey:             os.Getenv("TLS_KEY"),
 	}
 
 	mux = pat.New()
@@ -44,6 +47,12 @@ func init() {
 		config.addr = defaultAddr
 	}
 
+	if os.Getenv("ALLOWED_HOSTS") == "" {
+		config.allowedHosts = nil
+	} else {
+		config.allowedHosts = strings.Split(os.Getenv("ALLOWED_HOSTS"), ",")
+	}
+
 	model.SetLogger(log)
 	model.InitDB("sqlite3", "sqlite.db")
 
@@ -52,14 +61,31 @@ func init() {
 }
 
 func main() {
+	secureOpts := secure.Options{
+		AllowedHosts:          config.allowedHosts,
+		BrowserXssFilter:      true,
+		ContentSecurityPolicy: "default-src 'self' 'unsafe-inline'; img-src 'self' data:",
+		FrameDeny:             true,
+		STSIncludeSubdomains:  true,
+		STSSeconds:            365 * 24 * 60 * 60,
+	}
+	secureMiddleware := secure.New(secureOpts)
+
 	n := negroni.New()
 	n.Use(negroniLogrus.New(log)) // logger must be first middleware
 	n.Use(negroni.NewRecovery())
+	n.Use(negroni.HandlerFunc(secureMiddleware.HandlerFuncWithNext))
 	n.Use(negroni.NewStatic(http.Dir(assetsPath)))
 	n.UseHandler(mux)
 	registerRoutes()
 
 	log.Infof("Listening on %s", config.addr)
+
+	if config.allowedHosts != nil {
+		log.Infof("Allowed HTTP hosts: %q", config.allowedHosts)
+	} else {
+		log.Warningln(errAllHostsEnabled)
+	}
 
 	s := &http.Server{Addr: config.addr, Handler: n}
 
@@ -73,5 +99,9 @@ func main() {
 	}
 }
 
-const errNoTLSCertificate = "No TLS certficiate supplied. Consider setting TLS_CERT " +
-	"and TLS_KEY environment variables to enable TLS."
+const (
+	errAllHostsEnabled = "Accepting connections for all HTTP hosts. Consider setting the ALLOWED_HOSTS environment variable."
+
+       errNoTLSCertificate = "No TLS certficiate supplied. Consider setting TLS_CERT " +
+		"and TLS_KEY environment variables to enable TLS."
+)
