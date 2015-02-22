f2dbc01527fb3f8278177ce32c5003781c241e7c
Matt Bostock <matt@mattbostock.com>
2015-02-22 11:09:44 +0000

    Move test setup to main_test.go
    Move test setup common to all tests in the `handler` package to
    `main_test.go`, seems like a more logical place for it.

---
+++ handler/main_test.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"testing"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/gorilla/securecookie"
+	"github.com/stretchr/testify/suite"
+	"gitlab.com/mattbostock/timeoff/middleware/sessions"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/github"
+)
+
+type handlerTestSuite struct {
+	suite.Suite
+}
+
+func TestHandlerTests(t *testing.T) {
+	suite.Run(t, new(handlerTestSuite))
+}
+
+func (s *handlerTestSuite) SetupSuite() {
+	log = logrus.New()
+	oauthConfig = &oauth2.Config{
+		ClientID:     "abc",
+		ClientSecret: "xyz",
+		Endpoint:     github.Endpoint,
+		Scopes:       []string{"user:email"},
+	}
+	sessionManager = sessions.New("test_session", securecookie.GenerateRandomKey(32))
+}
