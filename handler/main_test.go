3555ee6f1905ad45103fbad5b97f31667be4be2f
Matt Bostock <matt@mattbostock.com>
2015-04-24 17:28:16 +0100

    Add tests for handler/oauth_callback.go
    Add tests for the GitHub Oauth HTTP handler.
    
    These tests stub out a GitHub Oauth response and the GitHub API.
    
    We also check that a new user has been created in the database when a
    GitHub user authenticates for the first time. I'm not 100% sure if
    checking the database in a unit test is the right thing, but it seems to
    be appropriate here. In any case, it's too early to think about
    optimising/refactoring this yet.

--- handler/main_test.go
+++ handler/main_test.go
@@ -1,6 +1,9 @@
 package handler
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"os"
 	"testing"
 
@@ -11,7 +14,6 @@ import (
 	"github.com/mattbostock/timeoff/model"
 	"github.com/stretchr/testify/suite"
 	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/github"
 )
 
 type handlerTestSuite struct {
@@ -27,14 +29,104 @@ func (s *handlerTestSuite) SetupTest() {
 	log = logrus.New()
 	s.log = &mockhook.Mockhook{}
 	log.Hooks.Add(s.log)
+	oauthServer := mockGithubServer()
+
+	var err error
+	githubAPIBaseURL, err = url.Parse(oauthServer.URL)
+	if err != nil {
+		panic(err)
+	}
 
 	oauthConfig = &oauth2.Config{
 		ClientID:     "abc",
 		ClientSecret: "xyz",
-		Endpoint:     github.Endpoint,
-		Scopes:       []string{"user:email"},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  oauthServer.URL + "/foo",
+			TokenURL: oauthServer.URL + "/login/oauth/access_token",
+		},
+		Scopes: []string{"user:email"},
 	}
 	sessionManager = sessions.New("test_session", securecookie.GenerateRandomKey(32))
+}
+
+func mockGithubServer() *httptest.Server {
+	// FIXME Split this into an Oauth server and a GitHub API server at some point
+	m := http.NewServeMux()
+	server := httptest.NewServer(m)
+
+	var accessTokenPayload = []byte(`access_token=sekret&scope=user%3Aemail&token_type=bearer`)
+	var userPayload = []byte(`
+{
+  "login": "octocat",
+  "id": 1,
+  "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/octocat",
+  "html_url": "https://github.com/octocat",
+  "followers_url": "https://api.github.com/users/octocat/followers",
+  "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+  "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+  "organizations_url": "https://api.github.com/users/octocat/orgs",
+  "repos_url": "https://api.github.com/users/octocat/repos",
+  "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/octocat/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": "monalisa octocat",
+  "company": "GitHub",
+  "blog": "https://github.com/blog",
+  "location": "San Francisco",
+  "email": "octocat@github.com",
+  "hireable": false,
+  "bio": "There once was...",
+  "public_repos": 2,
+  "public_gists": 1,
+  "followers": 20,
+  "following": 0,
+  "created_at": "2008-01-14T04:33:35Z",
+  "updated_at": "2008-01-14T04:33:35Z",
+  "total_private_repos": 100,
+  "owned_private_repos": 100,
+  "private_gists": 81,
+  "disk_usage": 10000,
+  "collaborators": 8,
+  "plan": {
+    "name": "Medium",
+    "space": 400,
+    "private_repos": 20,
+    "collaborators": 0
+  }
+}
+`)
+	var userEmailsPayload = []byte(`
+[
+  {
+    "email": "octocat@github.com",
+    "verified": true,
+    "primary": true
+  }
+]
+`)
+
+	m.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/login/oauth/access_token":
+			w.Write(accessTokenPayload)
+			return
+		case "/user":
+			w.Write(userPayload)
+			return
+		case "/user/emails":
+			w.Write(userEmailsPayload)
+			return
+		default:
+			panic("Request received at unexpected endpoint: " + r.URL.Path)
+		}
+	})
+
+	return server
 }
 
 func setupTestDB() {
---
+++ handler/oauth_callback_test.go
@@ -0,0 +1,73 @@
+package handler
+
+import (
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/mattbostock/timeoff/model"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/oauth2"
+)
+
+func (s *handlerTestSuite) TestOauthCallbackStateMismatch() {
+	r, err := http.NewRequest("GET", "https://localhost/?state=1234", nil)
+	if err != nil {
+		s.T().Error(err)
+	}
+
+	r.AddCookie(&http.Cookie{Name: oauthStateCookieName, Value: "4321"})
+	w := httptest.NewRecorder()
+
+	GithubOauthCallback(w, r)
+
+	assert.Equal(s.T(), "GitHub state mismatch during Oauth callback", s.log.LastEntry().Message)
+	assert.Equal(s.T(), http.StatusTemporaryRedirect, w.Code)
+	assert.Equal(s.T(), "/", w.Header().Get("Location"))
+}
+
+func (s *handlerTestSuite) TestOauthExchangeFailed() {
+	r, err := http.NewRequest("GET", "https://localhost/?state=1234", nil)
+	if err != nil {
+		s.T().Error(err)
+	}
+
+	r.AddCookie(&http.Cookie{Name: oauthStateCookieName, Value: "1234"})
+	w := httptest.NewRecorder()
+
+	// Use an Oauth config that should fail the Oauth exchange
+	oauthConfig.Endpoint = oauth2.Endpoint{"https://127.0.0.1/404", "https://127.0.0.1/404"}
+
+	GithubOauthCallback(w, r)
+
+	assert.Contains(s.T(), s.log.LastEntry().Message, "GitHub Oauth exchange failed")
+	assert.Equal(s.T(), http.StatusTemporaryRedirect, w.Code)
+	assert.Equal(s.T(), "/", w.Header().Get("Location"))
+}
+
+func (s *handlerTestSuite) TestGitHubAuthenticationNewUser() {
+	r, err := http.NewRequest("GET", "https://localhost/?state=1234", nil)
+	if err != nil {
+		s.T().Error(err)
+	}
+
+	r.AddCookie(&http.Cookie{Name: oauthStateCookieName, Value: "1234"})
+	w := httptest.NewRecorder()
+
+	setupTestDB()
+
+	GithubOauthCallback(w, r)
+
+	u, err := model.FindUser(1)
+	if err != nil {
+		panic(err)
+	}
+
+	assert.Equal(s.T(), "octocat@github.com", u.Email)
+	assert.Equal(s.T(), "monalisa octocat", u.Name)
+	assert.Equal(s.T(), 1, u.GitHubID)
+
+	assert.Equal(s.T(), http.StatusTemporaryRedirect, w.Code)
+	assert.Equal(s.T(), "/dashboard", w.Header().Get("Location"))
+
+	assert.Len(s.T(), s.log.Entries, 0)
+}
