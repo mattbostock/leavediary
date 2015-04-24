ef9e18bf261c34c5133bb30c75c86e133b798ef1
Matt Bostock <matt@mattbostock.com>
2015-04-24 17:07:18 +0100

    Add support for PostgreSQL as database
    Add support for PostgreSQL, which is configured using the `DB_DIALECT`
    and `DB_DATASOURCE` environment variables.
    
    The default remains as an in-memory SQLite3 database, which is useful
    for someone wishing to try out the application without having to
    configure a database.
    
    I plan to support both SQLite and PostgreSQL for now. If that becomes
    too difficult, I'll drop support for SQLite later.
    
    If PostgreSQL is configured using the environment variables, the test
    suite will run against PostgreSQL rather than SQLite. I've configured
    Travis CI to run tests against both.

--- handler/main_test.go
+++ handler/main_test.go
@@ -1,11 +1,13 @@
 package handler
 
 import (
+	"os"
 	"testing"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/gorilla/securecookie"
 	"github.com/mattbostock/timeoff/middleware/sessions"
+	"github.com/mattbostock/timeoff/model"
 	"github.com/stretchr/testify/suite"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/github"
@@ -28,4 +30,17 @@ func (s *handlerTestSuite) SetupSuite() {
 		Scopes:       []string{"user:email"},
 	}
 	sessionManager = sessions.New("test_session", securecookie.GenerateRandomKey(32))
+}
+
+func setupTestDB() {
+	dbDialect := os.Getenv("DB_DIALECT")
+	dbDataSource := os.Getenv("DB_DATASOURCE")
+
+	if dbDialect == "" && dbDataSource == "" {
+		dbDialect = "sqlite3"
+		dbDataSource = ":memory:"
+	}
+
+	model.SetLogger(log)
+	model.InitDB(dbDialect, dbDataSource)
 }
